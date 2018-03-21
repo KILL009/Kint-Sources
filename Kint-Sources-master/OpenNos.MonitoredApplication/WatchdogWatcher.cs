edd5764f627f949d25166c2811625adad22297ff
JOAOMASTER <thecracy45@hotmail.com>
2018-03-21 07:26:27 -0400

    Fixes

--- Kint-Sources-master/OpenNos.MonitoredApplication/WatchdogWatcher.cs
+++ Kint-Sources-master/OpenNos.MonitoredApplication/WatchdogWatcher.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MonitoredApplication
 {
