b90c397ac052b1076e0b24ec728127d68d4e0b42
KILL009 <gmthemaster@gmail.com>
2018-09-16 15:39:02 -0400

    Add PrestigeLvl command

---
+++ Kint-Sources-master/OpenNos.GameObject/Packets/CommandPackets/ChangeprestigeLevelPacket.cs
@@ -0,0 +1,21 @@
+﻿using OpenNos.Core;
+using OpenNos.Domain;
+
+namespace OpenNos.GameObject.CommandPackets
+{
+    [PacketHeader("$PrestigeLvl", PassNonParseablePacket = true, Authority = AuthorityType.GameMaster)]
+    public class ChangeprestigeLevelPacket : PacketDefinition
+    {
+        #region Properties
+
+        [PacketIndex(0)]
+        public byte prestigeLevel { get; set; }
+
+        public static string ReturnHelp()
+        {
+            return "$PrestigeLvl PRESTIGELEVEL";
+        }
+
+        #endregion
+    }
+}
