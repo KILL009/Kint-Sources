0c3970004cfed5f02fa69d9b9d30a3975701078f
ZroFreaks <toniko15@outlook.com>
2019-04-28 17:13:10 +0200

    Remove

--- Kint-Sources-master/OpenNos.Master.Server/Properties/AssemblyInfo.cs
+++ Kint-Sources-master/OpenNos.Master.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden Attribute gesteuert. Ändern
 // Sie diese Attributwerte, um die Informationen zu ändern, die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("OpenNos Master Server")]
-[assembly: AssemblyDescription("Source# Master")]
+[assembly: AssemblyDescription("ZroFreaks Master")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Source# Inc.")]
+[assembly: AssemblyCompany("ZroFreaks Inc.")]
 [assembly: AssemblyProduct("OpenNos.Master.Server")]
-[assembly: AssemblyCopyright("Source# Copyright ©  2019")]
+[assembly: AssemblyCopyright("ZroFreaks Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
